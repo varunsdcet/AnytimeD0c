910a9e15af95992fbdc4b1e62c5a1e9bb760d452
varunsdcet <33832368+varunsdcet@users.noreply.github.com>
2019-10-03 10:51:19 +0530

    Add files via upload

---
+++ Insurance.js
@@ -0,0 +1,281 @@
+import React, {Component} from 'react';
+import {
+    StyleSheet,
+    Text,
+    TextInput,
+    View,
+    Image,
+    Alert,
+
+    FlatList,
+    Dimensions,
+    TouchableOpacity,
+    ActivityIndicator,
+    SafeAreaView,
+    AsyncStorage
+} from 'react-native';
+import Button from 'react-native-button';
+const window = Dimensions.get('window');
+import Voice from 'react-native-voice';
+const GLOBAL = require('./Global');
+import { TextField } from 'react-native-material-textfield';
+type Props = {};
+
+let customDatesStyles = [];
+
+import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view';
+
+
+export default class Insurance extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            recognized: '',
+            started: '',
+            results: [],
+            images: [
+            ]
+
+        };
+
+    }
+
+
+
+
+
+
+    static navigationOptions = ({ navigation }) => {
+        return {
+            //   header: () => null,
+            title: 'INSURANCE',
+            headerTitleStyle :{textAlign: 'center',alignSelf:'center',color :'black'},
+            headerStyle:{
+                backgroundColor:'white',
+            },
+            headerTintColor :'#0592CC',
+            animations: {
+                setRoot: {
+                    waitForRender: false
+                }
+            }
+        }
+    }
+
+
+
+    showLoading() {
+        this.setState({loading: true})
+    }
+
+
+    hideLoading() {
+        this.setState({loading: false})
+    }
+
+
+    componentDidMount(){
+        this.showLoading()
+
+        const url = GLOBAL.BASE_URL +  'insurance_for_hospital'
+
+        fetch(url, {
+            method: 'GET',
+
+
+
+        }).then((response) => response.json())
+            .then((responseJson) => {
+
+                this.hideLoading()
+
+                if (responseJson.status == true) {
+                    this.setState({results:responseJson.insurence})
+
+                }
+            })
+            .catch((error) => {
+                console.error(error);
+                this.hideLoading()
+            });
+        //   this._handlePressLogin()
+    }
+
+
+    selectedFirst = (item) => {
+        GLOBAL.hid = item.id
+        this.props.navigation.navigate('HospitalList')
+    }
+
+
+
+    _renderItems = ({item,index}) => {
+
+        return (
+            <TouchableOpacity onPress = {()=>this.selectedFirst(item)} >
+
+
+                <View style={{ flex: 1 ,marginLeft : 5,width:window.width - 10, backgroundColor: 'white',marginTop: 10,marginBottom:10,borderRadius:10}}>
+
+
+                            <Text style={{marginLeft : 5,fontSize : 18,color :'#3A3A3A',fontFamily:'Poppins-Medium',width :window.width -120,marginTop:8,marginBottom:8}}>
+
+                                {item.insurance_name}
+                            </Text>
+
+
+
+                </View>
+            </TouchableOpacity>
+
+
+        )
+    }
+    render() {
+
+
+        let { phone } = this.state;
+        let { email } = this.state;
+        let { name } = this.state;
+        let { company } = this.state;
+        if(this.state.loading){
+            return(
+                <View style={styles.container}>
+                    <ActivityIndicator style = {styles.loading}
+
+                                       size="large" color='#006FA5' />
+                </View>
+            )
+        }
+        return (
+            <SafeAreaView>
+                <View style={styles.container}>
+
+                    <View style = {{margin :10,width:window.width - 20 ,height:35,borderRadius:20,flexDirection:'row',backgroundColor:'white',}}>
+
+                        <Image style = {{width :18 ,height: 18,alignSelf:'center',resizeMode: 'contain',marginLeft:13}}
+                               source={require('./search.png')}/>
+
+                        <TextInput style={{marginLeft:10 ,width:window.width - 100}}
+                                   placeholderTextColor='rgba(0, 0, 0, 0.4)'
+                                   onChangeText={(text) => this.setState({height:text})
+
+                                   } placeholder={"Search"}/>
+
+
+
+
+                    </View>
+
+
+
+
+
+                    <FlatList style= {{flexGrow:0,margin:8}}
+                              data={this.state.results}
+                              numColumns={1}
+                              keyExtractor = { (item, index) => index.toString() }
+                              renderItem={this._renderItems}
+                    />
+
+
+
+
+
+
+
+
+
+
+
+                </View>
+            </SafeAreaView>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    wrapper: {
+    },
+    container: {
+
+        backgroundColor :'#f1f1f1',
+
+    },
+    loading: {
+        position: 'absolute',
+        left: window.width/2 - 30,
+
+        top: window.height/2,
+
+        opacity: 0.5,
+
+        justifyContent: 'center',
+        alignItems: 'center'
+    },
+    slide1: {
+
+        marginLeft : 50,
+
+        width: window.width - 50,
+        height:300,
+        resizeMode:'contain',
+        marginTop : window.height/2 - 200
+
+
+    },
+    slide2: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    slide3: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#92BBD9',
+    },
+    text: {
+        color: '#fff',
+        fontSize: 30,
+        fontWeight: 'bold',
+    },
+    account :{
+        marginTop : 20,
+        textAlign : 'center',
+        fontSize: 17,
+        justifyContent:'center',
+        color : '#262628',
+        fontFamily:'Poppins-Regular',
+
+
+    } ,
+    createaccount :{
+        marginLeft : 5,
+        fontSize: 17,
+        textAlign : 'center',
+        marginTop : 30,
+        color : '#0592CC',
+
+
+
+
+    } ,
+
+    createaccounts :{
+        marginLeft : 5,
+        fontSize: 17,
+        textAlign : 'center',
+        marginTop : 30,
+        color : '#0592CC',
+        textDecorationLine: 'underline',
+
+
+
+    } ,
+    transcript: {
+        textAlign: 'center',
+        color: 'red',
+
+    },
+})
